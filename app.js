bbd1a9e375fac487187ff2ed6a907caf97e31abd
Jonathan Ligh <JonathankLigh@gmail.com>
2016-06-20 11:45:02 -0400

    swig and morgan installed

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 'use strict';
 var express = require('express');
-var app = express();
+var app = new express();
+var routes = require('./routes');
+var morgan = require('morgan');
+var swig = require('swig');
+
+app.engine('html', swig.renderFile);
+app.use(morgan('dev'));
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+
+var server = app.listen(3000);
