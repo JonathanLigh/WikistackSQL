44bbc8e03cb1ecd7ffb73774274189c78d8b67d1
Jonathan Ligh <JonathankLigh@gmail.com>
2016-06-20 12:18:02 -0400

    db created

--- models/index.js
+++ models/index.js
@@ -11,18 +11,24 @@ var pageObj = {
 	content: Sequelize.TEXT,
 	date: Sequelize.DATE,
 	status: Sequelize.BOOLEAN
-}
+};
 
 var userObj = {
-	name: Sequelize.STRING
+	username: Sequelize.STRING
 	email: Sequelize.String,
-
-}
-
-
-
-var Page = db.define('Page', pageObj)
+};
 
 
 
-var User = db.define('User', userObj)
+var Page = db.define('Page', pageObj);
+var User = db.define('User', userObj);
+User.hook
+
+db
+	.sync({ force: true})
+	.the(function(err){
+		console.log('it works');
+	}, function(err){
+		console.error(err);
+	});
+
